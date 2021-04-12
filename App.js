e0dc7003b7256af2ae2a1f64c69d1e37e2b701d9
PrinceSP <princepasombaran@gmail.com>
2021-04-12 14:33:37 +0800

    registration form (console.log)

---
+++ App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import {StyleSheet,Text,View,ScrollView} from 'react-native';
+import Forms from './src/components/form'
+
+
+const App = ()=>{
+  return(
+    <ScrollView>
+      <Forms/>
+    </ScrollView>
+  )
+}
+
+export default App;
